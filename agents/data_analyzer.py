45fbcb6c9ad0ebf95585fee421308d855098cddf
viraj deshmukh <virajdeshmukh080818@gmail.com>
2025-11-06 23:05:20 +0530

    add data analyzer agent

--- agents/data_analyzer.py
+++ agents/data_analyzer.py
@@ -2,9 +2,14 @@ from autogen_agentchat.agents import AssistantAgent
 from agents.prompts.DataAnalyzerPrompt import DATA_ANALYZER_MSG
 from config.model_client import get_model_client
 
-def GetDataAnalyzerAgent(model_client):
+model_client = get_model_client()
+
+def getDataAnalyzerAgent(model_client):
     data_analyzer_agent = AssistantAgent(
-    name = "Data_Analyzer",
-    description="An agent which helps with solving Data Analysis task and gives the cod as well",
-    model_client=model_client,
-    system_message=DATA_ANALYZER_MSG)
+        name = "Data_Analyzer_Agent",
+        description='An agent which helps with solving Data Analysis task and gives the code as well',
+        model_client=model_client,
+        system_message=DATA_ANALYZER_MSG
+    )
+
+    return data_analyzer_agent
